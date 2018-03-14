2ca55657bc70de6171f0ef2a942bb8ec09627b28
Marcus Hopp <pvt_m4ster@gmx.de>
2018-03-14 19:38:23 +0100

    Initial Commit

---
+++ decruiter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  decruiter
+//
+//  Created by Marcus on 14.03.18.
+//  Copyright © 2018 Marcus Hopp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
