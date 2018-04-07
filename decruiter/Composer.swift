3f653982ead478dda0e129b948cdc00305a9d1ab
mrc5 <mrc5@gmx.de>
2018-04-07 14:00:29 +0200

    Added composer

---
+++ decruiter/Composer.swift
@@ -0,0 +1,14 @@
+//
+//  Composer.swift
+//  decruiter
+//
+//  Created by Marcus on 07.04.18.
+//  Copyright © 2018 Marcus Hopp. All rights reserved.
+//
+
+import Foundation
+
+
+class Composer {
+    
+}
