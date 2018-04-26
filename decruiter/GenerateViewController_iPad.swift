c36f638296305c39c016fe78113b9b01bc2a4694
mrc5 <mrc5@gmx.de>
2018-04-26 20:14:56 +0200

    Resolved appicon, resolved ipad crash

---
+++ decruiter/GenerateViewController_iPad.swift
@@ -0,0 +1,20 @@
+//
+//  GenerateViewController_iPad.swift
+//  decruiter
+//
+//  Created by Marcus on 26.04.18.
+//  Copyright © 2018 Marcus Hopp. All rights reserved.
+//
+
+import UIKit
+
+class GenerateViewController_iPad: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
